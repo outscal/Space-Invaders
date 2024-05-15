23110b6b10bf31b4c4aa58da9f439fa85c1d8c48
Avnish <avnishkr200125@gmail.com>
2024-05-15 18:41:18 +0530

    Window creation done

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,42 @@
+#include <SFML/Graphics.hpp>
 
-int main()
-{
+int main() {
+
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = *(new sf::VideoMode(1280, 720));
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
+
+    //Enable fullscreen mode:
+    //sf::RenderWindow* window = new sf::RenderWindow(videoMode, "Fullscreen Window",sf::Style::Fullscreen);
+
+
+    // Game loop to keep the window open
+    while (window->isOpen()) {
+        //Set Framerate limit
+        //window->setFramerateLimit(60);
+
+        //Set window position on screen
+        //window->setPosition(sf::Vector2i(100, 100));
+
+        //Event object
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+
+        // Clear the window
+        window->clear(sf::Color::Blue);
+
+        // Draw your content here...
+
+        // Display what was drawn
+        window->display();
+    }
+
     return 0;
 }
