8f5e6e11e1f328f08d65d47b49d81cd1a7087566
Avnish <avnishkr200125@gmail.com>
2024-05-16 16:40:05 +0530

    Drawing shapes: Red square,blue triangle,green circle.

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -20,6 +20,8 @@ int main() {
         //Set window position on screen
         //window->setPosition(sf::Vector2i(100, 100));
 
+
+
         //Event object
         sf::Event event;
         while (window->pollEvent(event))
@@ -28,11 +30,39 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window->close();
         }
+       
 
         // Clear the window
-        window->clear(sf::Color::Blue);
+        window->clear(sf::Color::White);
 
         // Draw your content here...
+        
+        // Draw a shape
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Green);
+
+        sf::RectangleShape square(sf::Vector2f(100, 100));
+        square.setFillColor(sf::Color::Red);
+
+        sf::ConvexShape triangle;
+        triangle.setPointCount(3);
+        triangle.setFillColor(sf::Color::Blue);
+        
+        // Set position
+        //circle.setPosition(640,360); 
+        circle.setPosition(window->getSize().x / 2 - circle.getRadius(), window->getSize().y / 2 - circle.getRadius());
+        
+        square.setPosition(300, 300);
+        
+        
+        triangle.setPoint(0, sf::Vector2f(100, 100));//left point
+        triangle.setPoint(1, sf::Vector2f(200, 300));//bottom point
+        triangle.setPoint(2, sf::Vector2f(300, 100));//right point
+
+        //Queue for display
+        window->draw(circle);
+        window->draw(square);
+        window->draw(triangle);
 
         // Display what was drawn
         window->display();
