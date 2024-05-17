92330df600b7deb7b69fdb7506e532c9aebec47f
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-17 18:02:11 +0530

    Create an entry point for your game

--- Space-Invaders/Header/GameService.h
+++ Space-Invaders/Header/GameService.h
@@ -1,1 +1,21 @@
 #pragma once
+
+class GameService {
+private:
+	void initialize() {};
+	void destroy() {};
+
+public:
+	GameService() {
+
+	}
+
+	~GameService() {
+
+	}
+	void ignite() {};
+	void update() {};
+	void render() {};
+	bool isRunning() {};
+	
+};
--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,89 +1,97 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include "Header/GameService.h"
 
-class Player {
-    
-private:
-    int playerScore=0;
-    int health=100;
-    int movementSpeed=1;
-    sf::Vector2f position= sf::Vector2f(200,100);
-
-public:
-
-    sf::Texture playerTexture;
-    sf::Sprite playerSprite;
-
-    int getScore() {
-        return playerScore;
-    }
-    int getMoveSpeed() {
-        return movementSpeed;
-    }
-
-    sf::Vector2f getPosition() {
-        return position;
-    }
-
-    void setScore(int newScore) {
-        this->playerScore = newScore;
-    }
-
-    void takeDamage() {
-
-    }
-
-    void move(int offsetX) {
-        position.x += offsetX;
-    }
-
-    void shootBullets() {
-
-    }
-};
+//class Player {
+//    
+//private:
+//    int playerScore=0;
+//    int health=100;
+//    int movementSpeed=1;
+//    sf::Vector2f position= sf::Vector2f(200,100);
+//
+//public:
+//
+//    sf::Texture playerTexture;
+//    sf::Sprite playerSprite;
+//
+//    int getScore() {
+//        return playerScore;
+//    }
+//    int getMoveSpeed() {
+//        return movementSpeed;
+//    }
+//
+//    sf::Vector2f getPosition() {
+//        return position;
+//    }
+//
+//    void setScore(int newScore) {
+//        this->playerScore = newScore;
+//    }
+//
+//    void takeDamage() {
+//
+//    }
+//
+//    void move(int offsetX) {
+//        position.x += offsetX;
+//    }
+//
+//    void shootBullets() {
+//
+//    }
+//};
 
 
 int main()
 {
-    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
+    //sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
 
-    // Create a window object with specific dimensions and a title
-    sf::RenderWindow window(videoMode, "My SFML Window");
-    Player player;
-    player.playerTexture.loadFromFile("assets/textures/player_ship.png");
-    player.playerSprite.setTexture(player.playerTexture);
-    
-    
+    //// Create a window object with specific dimensions and a title
+    //sf::RenderWindow window(videoMode, "My SFML Window");
+    //Player player;
+    //player.playerTexture.loadFromFile("assets/textures/player_ship.png");
+    //player.playerSprite.setTexture(player.playerTexture);
+    //
+    //
 
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            // Check for window closure
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    //while (window.isOpen())
+    //{
+    //    sf::Event event;
+    //    while (window.pollEvent(event)) {
+    //        // Check for window closure
+    //        if (event.type == sf::Event::Closed)
+    //            window.close();
+    //    }
 
-       
-      
-       
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            player.move(-1.0f*player.getMoveSpeed());
-        }
+    //   
+    //  
+    //   
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+    //        player.move(-1.0f*player.getMoveSpeed());
+    //    }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            player.move(1.0f* player.getMoveSpeed());
-        }
-        // Clear the window
-        window.clear(sf::Color::Blue);
-        // Draw your content here...
-        player.playerSprite.setPosition(player.getPosition());
-        window.draw(player.playerSprite);
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+    //        player.move(1.0f* player.getMoveSpeed());
+    //    }
+    //    // Clear the window
+    //    window.clear(sf::Color::Blue);
+    //    // Draw your content here...
+    //    player.playerSprite.setPosition(player.getPosition());
+    //    window.draw(player.playerSprite);
 
-        // Display what was drawn
-        window.display();
+    //    // Display what was drawn
+    //    window.display();
+    //}
+
+
+    GameService gameserice;
+    gameserice.ignite();
+    if (gameserice.isRunning() ){
+        gameserice.update();
+        gameserice.render();
     }
-    return 0;
 
 }
