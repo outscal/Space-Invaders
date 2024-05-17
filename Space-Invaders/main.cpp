5ee9eae8150f9bc213386de97650863ca5bed6f7
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-17 16:21:17 +0530

    Fetch Keyboard Inputs

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -18,6 +18,10 @@ public:
         return playerScore;
     }
 
+    sf::Vector2f getPosition() {
+        return position;
+    }
+
     void setScore(int newScore) {
         this->playerScore = newScore;
     }
@@ -41,28 +45,41 @@ int main()
     sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
 
     // Create a window object with specific dimensions and a title
-    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
+    sf::RenderWindow window(videoMode, "My SFML Window");
+    Player player;
+    player.playerTexture.loadFromFile("assets/textures/player_ship.png");
+    player.playerSprite.setTexture(player.playerTexture);
+    
+    
 
 
-    while (window->isOpen())
+    while (window.isOpen())
     {
         sf::Event event;
-        while (window->pollEvent(event)) {
+        while (window.pollEvent(event)) {
             // Check for window closure
             if (event.type == sf::Event::Closed)
-                window->close();
+                window.close();
         }
 
-        // Clear the window
-        window->clear(sf::Color::Blue);
-
-        // Draw your content here...
-        
-
        
+      
+       
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            player.move();
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            player.move();
+        }
+        // Clear the window
+        window.clear(sf::Color::Blue);
+        // Draw your content here...
+        player.playerSprite.setPosition(player.getPosition());
+        window.draw(player.playerSprite);
 
         // Display what was drawn
-        window->display();
+        window.display();
     }
     return 0;
 
