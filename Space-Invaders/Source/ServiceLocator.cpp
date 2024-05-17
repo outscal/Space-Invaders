6495fb7ca24a0421352f7221ccff5653bd193ea1
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-17 18:12:32 +0530

    Create Service Locator

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,34 @@
+#include "../Header/ServiceLocator.h"
+
+ServiceLocator::ServiceLocator()
+{
+}
+
+ServiceLocator::~ServiceLocator()
+{
+}
+
+void ServiceLocator::createServices()
+{
+}
+
+void ServiceLocator::clearAllServices()
+{
+}
+
+ServiceLocator* ServiceLocator::getInstance()
+{
+	return nullptr;
+}
+
+void ServiceLocator::initialize()
+{
+}
+
+void ServiceLocator::update()
+{
+}
+
+void ServiceLocator::render()
+{
+}
