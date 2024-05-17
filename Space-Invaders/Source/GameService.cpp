751409832c69419519c0f6b58fa313309a684d8a
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-17 17:35:39 +0530

    Create the Game Loop

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "../Header/GameService.h"
+
+void GameService::initialize() {
+	//Get things running
+}
+
+void GameService::destroy() {
+	//destroys things
+}
+
+GameService::GameService(){
+	//Constructor
+}
+
+GameService::~GameService() {
+	//Destructor
+}
+
+void GameService::ignite() {
+	//starts the game
+}
+
+void GameService::update() {
+	//Updates the game logic and game state.
+}
+
+void GameService::render() {
+	//Renders each frame of the game.
+}
+
+bool GameService::isRunning() {
+	//Checks if the game is currently running.
+	return false;
+}
+
