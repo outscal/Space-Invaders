8e4ae0c9440d4c04e4c5ba3413e69469a14b3d26
Avnish <avnishkr200125@gmail.com>
2024-05-22 03:19:22 +0530

    handling input for player

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Player
 {   //Properties
     public:
-    sf::Texture ship_texture;
+    sf::Texture ship_Texture;
     sf::Sprite ship_Sprite;
     int health;
     int movementSpeed = 10;
@@ -18,27 +18,31 @@ class Player
 
 };
 
-int main() {
+int main() 
+{
 
     // Define the video mode (dimensions)
-    sf::VideoMode videoMode = *(new sf::VideoMode(1280, 720));
+    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
 
     // Create a window object with specific dimensions and a title
     sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
 
+    //Player Object
+    Player playerObj;
+    playerObj.ship_Texture.loadFromFile("C:/Users/avnis/OneDrive/Desktop/Avnish Space Invaders/Space-Invaders-SFML/Space-Invaders/assets/textures/player_ship.png");
+    playerObj.ship_Sprite.setTexture(playerObj.ship_Texture);
     //Enable fullscreen mode:
     //sf::RenderWindow* window = new sf::RenderWindow(videoMode, "Fullscreen Window",sf::Style::Fullscreen);
 
 
     // Game loop to keep the window open
-    while (window->isOpen()) {
+    while (window->isOpen()) 
+    {
         //Set Framerate limit
         window->setFramerateLimit(60);
 
         //Set window position on screen
         //window->setPosition(sf::Vector2i(100, 100));
-
-
 
         //Event object
         sf::Event event;
@@ -48,19 +52,30 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window->close();
         }
-       
 
-        // Clear the window
+
+        //Handle Input
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            //call move here after player obj
+            playerObj.Move();
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            //call move here after player obj
+            playerObj.Move();
+        }
+        
+        //Clear Window
         window->clear(sf::Color::Blue);
 
-        sf::Texture outscal_texture;
-        outscal_texture.loadFromFile("C:/Users/avnis/OneDrive/Desktop/Avnish Space Invaders/Space-Invaders-SFML/Space-Invaders/assets/textures/outscal_logo.png");
-        Player player;
+        playerObj.ship_Sprite.setPosition(playerObj.position); // Set the position of the player sprite
 
-        std::cout << player.movementSpeed << endl;
-       
-        window->display();
-    }
+        window->draw(playerObj.ship_Sprite);    // Draw the player sprite
 
-    return 0;
+        window->display(); // Display what was drawn
+    } //while loop end.
+   
+
+    
 }
