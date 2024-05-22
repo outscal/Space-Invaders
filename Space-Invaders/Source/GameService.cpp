32971588b77d7d73f92e665218f24b46db71a58f
Avnish <avnishkr200125@gmail.com>
2024-05-22 15:42:28 +0530

    Added headers and source folder

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,29 @@
+#include "C:\Users\avnis\OneDrive\Desktop\Avnish Space Invaders\Space-Invaders-SFML\Space-Invaders\Headers\GameService.h"
+// Handles game initialization.
+void GameService::initialize()	
+{
+
+}
+// Handles cleanup tasks.
+void GameService::destroy()
+{
+
+}
+// Constructor for initializing the GameService object.
+GameService::GameService()
+{
+
+}
+// Destructor for cleaning up resources upon object deletion.
+GameService::~GameService()
+{
+
+}
+// Initiates the game.
+void GameService::ignite(){}
+// Updates the game logic and game state.
+void GameService::update(){}
+// Renders each frame of the game.
+void GameService::render(){}
+// Checks if the game is currently running.
+bool GameService::isRunning(){}
