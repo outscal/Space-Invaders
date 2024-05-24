d7c1f74c033f7f292dbfb221295fab20163bf5fd
Mirza_4565 <mirzaalishaik2000@gmail.com>
2024-05-24 13:11:18 +0530

    [ Player class , properties and methods ]

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -3,6 +3,37 @@
 
 class Player {
 
+    // Player properties
+   //Private Access Modifiers
+     int player_score = 0;
+     sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+     int health = 3;
+   
+public: //Public Access Modifier
+     sf::Texture player_texture;
+     sf::Sprite player_sprite;
+     int movement_speed = 5;
+
+   
+     //// Public Getter & Setter methods
+   //  int getScore() {
+   //      return player_score;
+   //  }
+  
+   //  void setScore(int newScoer) {
+   //  
+   //      player_score = newScoer;
+   //  
+   //  }
+
+
+     //New methods
+     void takeDamage() {};
+     void move() {};
+     void shootBullets() {};
+
+     
+
 };
 
 
@@ -11,11 +42,24 @@ class Player {
 
 int main()
 {
+    // A Player Object
+    Player player;
+
+    //// Accessing the private variables using the public getter
+    //std::cout << " Players Score " << player.getScore() << "\n";
+    //
+    //// Modifying the variables using the public setter
+    //player.setScore(100);
+
+    ////Acessing the modified variable using the public getter
+    //std::cout << " Player Modified scosre. " << player.getScore() << "\n";
+
+
     //Difining the video mode dimensions
     sf::VideoMode videoMode = sf::VideoMode(800, 600);
     
     // creating a window object with specific dimensions and a title
-    sf::RenderWindow window(videoMode, "SFML Window ");
+    sf::RenderWindow window(videoMode, " SFML Window ");
 
     while (window.isOpen())
     {
