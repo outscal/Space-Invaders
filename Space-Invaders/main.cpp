4d2ea485de7ef445456287dd621ead72a32648e5
Mirza_4565 <mirzaalishaik2000@gmail.com>
2024-05-24 12:54:04 +0530

    [ Created a Player class. ]

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,55 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+
+class Player {
+
+};
+
+
+
+
 
 int main()
 {
+    //Difining the video mode dimensions
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+    
+    // creating a window object with specific dimensions and a title
+    sf::RenderWindow window(videoMode, "SFML Window ");
+
+    while (window.isOpen())
+    {
+    sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+
+                window.close();
+        }
+
+        // Clear the  window
+        window.clear(sf::Color::Blue);
+
+        // Display whatever you draw
+        window.display();
+
+
+
+
+
+
+
+
+
+    }
+
+
+    
+
+
+
+
+
     return 0;
 }
