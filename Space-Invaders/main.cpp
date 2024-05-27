486def26d4e5472d4dc89776b05d95992f93817f
AyaanRawtani <ayaanrawtani@gmail.com>
2024-05-27 21:30:48 +0530

    drawing shapes

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -16,6 +16,29 @@ int main()
 
         window->clear(sf::Color::Blue);
 
+        sf::CircleShape circle(50);
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(50, 100);
+        window->draw(circle);
+
+        sf::RectangleShape(rectangle);
+        rectangle.setSize(sf::Vector2f(100, 100));
+        rectangle.setFillColor(sf::Color::Red);
+        rectangle.setPosition(300,100);
+        window->draw(rectangle);
+
+        sf::ConvexShape triangle;
+        triangle.setPointCount(3);
+        triangle.setPoint(0, sf::Vector2f(90, 0));
+        triangle.setPoint(1, sf::Vector2f(0, 90));
+        triangle.setPoint(2, sf::Vector2f(90, 180));
+        triangle.setFillColor(sf::Color::Magenta);
+        triangle.setPosition(550,50);
+        window->draw(triangle);
+
+
+
+
         window->display();
     }
     return 0;
