e218f20a9ce0e90e4eb9183cd03ab44c5882b91d
Abhaymodi860 <modiabhay860@gmail.com>
2024-05-27 23:18:09 +0530

    shapes commit

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -21,7 +21,34 @@ int main()
         window->clear(sf::Color::Yellow);
 
         // Draw Content here
+        //-------------------
+        // Draw circle
+        sf::CircleShape circle(50);  // radius 50
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(400, 300); // set position (x, y)
+        window->draw(circle);
 
+        // Create a rectangle shape
+        sf::RectangleShape rectangle(sf::Vector2f(100, 100));
+
+        // Set the position of the rectangle
+        rectangle.setPosition(10, 10);
+
+        rectangle.setFillColor(sf::Color::Red);
+
+        rectangle.setOutlineColor(sf::Color::Magenta);
+        rectangle.setOutlineThickness(5);
+        window->draw(rectangle);
+
+        sf::CircleShape Triangle(50, 3);  // radius 50
+        Triangle.setFillColor(sf::Color::Black);
+        Triangle.setPosition(700, 300); // set position (x, y)
+        window->draw(Triangle);
+
+       
+
+        // ------------------
+     
         //displat the content
         window->display();
     }
