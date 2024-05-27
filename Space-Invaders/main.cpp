b9370c22aff1efa4b86e78ca4a0cf32b3221dc24
AyaanRawtani <ayaanrawtani@gmail.com>
2024-05-27 21:08:26 +0530

    Created a blank window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,22 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
+    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "MY SFML Window");
+
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+
+        window->clear(sf::Color::Blue);
+
+        window->display();
+    }
     return 0;
 }
