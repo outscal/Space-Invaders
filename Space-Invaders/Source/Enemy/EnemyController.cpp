64e0eaee035fdab9088984fa1afec92d2da159cc
Avnish <avnishkr200125@gmail.com>
2024-05-27 17:10:51 +0530

    Bug Fix: Player state was not being set to right or left after moving down.

--- Space-Invaders/Source/Enemy/EnemyController.cpp
+++ Space-Invaders/Source/Enemy/EnemyController.cpp
@@ -48,18 +48,24 @@ namespace Enemy
 		else enemy_model->SetEnemyPosition(currentPosition);
 	}
 	void EnemyController::MoveLeft()
-	{
+	{	
+		// Get current pos
+		// Move left by multiplying by speed and delta time
 		sf::Vector2f currentPosition = enemy_model->GetEnemyPosition();
 		currentPosition.x -= enemy_model->enemy_movement_speed * ServiceLocator::GetInstance()->GetTimeService()->GetDeltaTime();
-		if (currentPosition.x >= enemy_model->left_most_position.x)
-		{	//if reached the left most pos
+
+		//check for bounds
+		if (currentPosition.x <= enemy_model->left_most_position.x)
+		{
+			//if reached the left most pos
 			//start moving down
 			//set ref pos for the downwards movement
-			enemy_model->SetMovementDirection(MovementDirection::DOWN); // move
-			enemy_model->SetReferencePosition(currentPosition);// set ref pos
+			enemy_model->SetMovementDirection(MovementDirection::DOWN);
+			enemy_model->SetReferencePosition(currentPosition);
 		}
 		else enemy_model->SetEnemyPosition(currentPosition);
 	}
+
 	void EnemyController::MoveDown()
 	{
 		sf::Vector2f currentPosition = enemy_model->GetEnemyPosition();
@@ -68,10 +74,16 @@ namespace Enemy
 		if (currentPosition.y >= enemy_model->GetReferencePosition().y + enemy_model->vertical_travel_distance)
 		{
 			if (enemy_model->GetReferencePosition().x <= enemy_model->left_most_position.x)
+			{
 				enemy_model->SetMovementDirection(MovementDirection::RIGHT);
-			else enemy_model->SetMovementDirection(MovementDirection::LEFT);
+			}
+				
+			if(enemy_model->GetReferencePosition().x >= enemy_model->right_most_position.x)
+			{ 
+				enemy_model->SetMovementDirection(MovementDirection::LEFT); 
+			}
 		}
-		else enemy_model->SetEnemyPosition(currentPosition);
+		else { enemy_model->SetEnemyPosition(currentPosition); }
 	}
 	//Lifecycle Methods
 	void EnemyController::Initialize()
