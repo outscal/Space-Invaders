1603c896fcab8047cec5c5eb46b1948a577f1463
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2024-05-28 10:51:19 -0400

    Done with shapes

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -18,7 +18,31 @@ int main()
             }
         }
 
-        window->clear(sf::Color::Blue);
+        window->clear(sf::Color::Yellow);
+
+        //Draw
+        //Circle
+        sf::CircleShape greensCircle(50);
+        greensCircle.setFillColor(sf::Color::Green);
+        greensCircle.setPosition(200, 300);
+        window->draw(greensCircle);
+
+
+        //Rectangle
+        sf::RectangleShape redSquare(sf::Vector2f(200.f,200.f));
+        redSquare.setFillColor(sf::Color::Red);
+        redSquare.setPosition(400, 300);
+        window->draw(redSquare);
+
+
+        //Blue Triangle
+        sf::ConvexShape blueTriangle;
+        blueTriangle.setPointCount(3);
+        blueTriangle.setPoint(0,sf::Vector2f(200,50));
+        blueTriangle.setPoint(1, sf::Vector2f(300, 200));
+        blueTriangle.setPoint(2, sf::Vector2f(100, 200));
+        blueTriangle.setFillColor(sf::Color::Blue);
+        window->draw(blueTriangle);
 
         window->display();
     }
