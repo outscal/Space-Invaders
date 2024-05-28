a837c74cdebfb5840435b581ff5f331c5f761ece
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2024-05-28 01:30:02 -0400

    Created Game Window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,27 @@
 
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
+
+    sf::RenderWindow*window = new sf::RenderWindow(videoMode, "My SFML Project");
+
+    //Game Loop
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window->close();
+            }
+        }
+
+        window->clear(sf::Color::Blue);
+
+        window->display();
+    }
+
     return 0;
 }
