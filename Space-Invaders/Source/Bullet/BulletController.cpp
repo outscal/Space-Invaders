9d291557e619cda25efac8f0759f920edb4476c3
Avnish <avnishkr200125@gmail.com>
2024-05-30 11:00:37 +0530

    Added Bullet MVC

---
+++ Space-Invaders/Source/Bullet/BulletController.cpp
@@ -0,0 +1,50 @@
+#include "C:\Users\avnis\OneDrive\Desktop\Avnish Space Invaders\Space-Invaders-SFML\Space-Invaders\Headers\Bullet\BulletController.h"
+namespace Bullet
+{
+	BulletController::BulletController(BulletType type)
+	{
+
+	}
+	BulletController::~BulletController()
+	{
+
+	}
+
+	void BulletController::UpdateProjectilePosition()
+	{
+
+	}
+	void BulletController::MoveUp()
+	{
+
+	}
+	void BulletController::MoveDown()
+	{
+
+	}
+	void BulletController::HandleOutOfBounds()
+	{
+
+	}
+	void BulletController::Initialize(sf::Vector2f position, Bullet::MovementDirection direction)
+	{
+
+	}
+	void BulletController::Update()
+	{
+
+	}
+	void BulletController::Render()
+	{
+
+	}
+
+	sf::Vector2f BulletController::GetProjectilePosition() 
+	{
+		return;
+	}
+	BulletType BulletController::GetBulletType()
+	{
+
+	}
+}
