ebb97ce5ca384552c14ccae3fff4fcc2a3a1ff47
AyaanRawtani <ayaanrawtani@gmail.com>
2024-06-03 11:23:07 +0530

    Added ServiceLocator

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,16 @@
+#include "ServiceLocator.h"
+
+ServiceLocator::ServiceLocator() {}
+ServiceLocator::~ServiceLocator() {}
+
+void ServiceLocator::createServices() {}
+void ServiceLocator::clearAllServices() {}
+
+ //... ServiceLocator::getInstance() {}
+ void ServiceLocator::initialize() {}
+ void ServiceLocator::update() {}
+ void ServiceLocator::render() {}
+ 
+
+
+
