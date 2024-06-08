c81929ee6ab07375c3bd7ed0da36747727d97196
Adnan Sayyed <adnan.sayyed.1947@gmail.com>
2024-06-08 12:48:24 +0530

    Drawn green circle, red square and blue triangle

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -15,7 +15,31 @@ int main() {
 				window->close();
 		}
 
-		window->clear(sf::Color::Blue);
+		window->clear(sf::Color::White);
+
+
+		sf::CircleShape circle(50); 
+		circle.setFillColor(sf::Color::Green);
+		circle.setPosition(300	, 100);
+
+		window->draw(circle);
+
+		sf::RectangleShape square(sf::Vector2f(100, 100));
+		square.setFillColor(sf::Color::Red);
+		square.setPosition(100, 100);
+		window->draw(square);
+
+
+
+		sf::ConvexShape triangle;
+		triangle.setPointCount(3);
+		triangle.setPoint(0,sf::Vector2f(0, 100));
+		triangle.setPoint(1, sf::Vector2f(50, 0));
+		triangle.setPoint(2, sf::Vector2f(100, 100));
+		triangle.setFillColor(sf::Color::Blue);
+		triangle.setPosition(500, 100);
+
+		window->draw(triangle);
 
 		window->display();
 	}
