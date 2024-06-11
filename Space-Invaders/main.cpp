e7d03ae4a1f53bd0e5d9659ea2529cfc1f4bdadf
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-06-11 12:17:13 +0530

    Commit

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,18 @@
+#include "header/GameService.h"
 
 int main()
 {
+
+    // comment all previous code in main()
+
+    GameService game_service; //create object
+    game_service.ignite(); // this will 'ignite' or start the flow of our game
+
+    // while the game is running we will update the game loop
+    while (game_service.isRunning())
+    {
+        game_service.update();
+        game_service.render();
+    }
     return 0;
 }
