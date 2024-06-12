9b0ac47efeb1bc99290ad6454be290200b67ef33
LowKey161 <adrien.gooding@gmail.com>
2024-06-12 10:55:01 -0400

    Add Outscal logo and "SFML is Awesome" text

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -33,11 +33,34 @@ int main()
         window->draw(rectangle);
 
 
-        //Draaw Triangle
+        //Draw Triangle
         sf::CircleShape triangle(80.f, 3);
         triangle.setFillColor(sf::Color::Magenta);
         triangle.setPosition(1130, 600);
         window->draw(triangle);
+
+
+        //loading texture
+        sf::Texture outscal_texture;
+        outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
+
+        //creating sprite
+        sf::Sprite outscal_sprite;
+        outscal_sprite.setTexture(outscal_texture);
+        outscal_sprite.setPosition(700, 100);
+        outscal_sprite.setRotation(45);
+        outscal_sprite.setScale(0.5, 0.5);
+        
+        window->draw(outscal_sprite);
+
+        sf::Font font;
+        font.loadFromFile("assets/fonts/OpenSans.ttf");
+        sf::Text text("SFML is Awesome", font, 50);
+        text.setFillColor(sf::Color::White);
+        text.setPosition(window->getSize().x/ 2-200, 600);
+
+        window->draw(text);
+        
 
         window->display();
 
