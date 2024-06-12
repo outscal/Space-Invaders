d8e4cc8aaed51d610de332ccbf324c0685edb996
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-06-12 06:27:44 +0530

    Commit

--- Space-Invaders/Header/GameService.h
+++ Space-Invaders/Header/GameService.h
@@ -1,39 +1,25 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "../Header/ServiceLocator.h"
+
 class GameService
 {
 private:
-	void initialize()
-	{
 
-	};		// Handles game initialization.
-	void destroy()
-	{
+	ServiceLocator* service_locator;
+	sf::RenderWindow* game_window;
 
-	};			// Handles cleanup tasks.
+	void initialize();
+	void initializeVariables();// Handles game initialization.
+	void destroy();			// Handles cleanup tasks.
 
 public:
-	GameService()
-	{
 
-	};			// Constructor for initializing the GameService object.
-	~GameService()
-	{
+	GameService();			// Constructor for initializing the GameService object.
+	~GameService();	// Destructor for cleaning up resources upon object deletion.
 
-	};	    // Destructor for cleaning up resources upon object deletion.
-
-	void ignite()
-	{
-
-	};// Initiates the game.
-	void update()
-	{
-
-	};			// Updates the game logic and game state.
-	void render()
-	{
-
-	};// Renders each frame of the game.
-	bool isRunning()
-	{
-
-	};
+	void ignite();			// Initiates the game.
+	void update();			// Updates the game logic and game state.
+	void render();			// Renders each frame of the game.
+	bool isRunning();		// Checks if the game is currently running.
+};
--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -2,17 +2,15 @@
 
 int main()
 {
+    GameService* game_service = new GameService();
 
-    // comment all previous code in main()
+    game_service->ignite();
 
-    GameService game_service; //create object
-    game_service.ignite(); // this will 'ignite' or start the flow of our game
-
-    // while the game is running we will update the game loop
-    while (game_service.isRunning())
+    while (game_service->isRunning())
     {
-        game_service.update();
-        game_service.render();
+        game_service->update();
+        game_service->render();
     }
+
     return 0;
 }
