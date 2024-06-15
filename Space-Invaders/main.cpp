13ce6635bf62b300d9072c40213a62938e75f42d
Deependra-Das <iamdeep75@gmail.com>
2024-06-15 08:19:58 +0530

    SFML Setup & Game Window
    - Added SFML dll files
    - Updated the code in the main file to create the game window using SFML renderWindow
    - Added a game loop in the main file to keep the game window from closing

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,39 @@
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <Windows.h>
+#include <string>
+
 
 int main()
 {
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow window(videoMode, "SFML Window");
+
+    window.setFramerateLimit(60);
+
+    window.setPosition(sf::Vector2i(100, 100));
+
+    //Creating a game loop to keep window from closing on its own
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        // Clear the window
+        window.clear(sf::Color::White);
+
+        // Display whatever you draw
+        window.display();
+    }
+
     return 0;
+
 }
