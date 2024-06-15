d4035be9a467e01bb5d4dfbf2cc6b1ae563aacb2
gamedevteam321 <vnshkumar21@gmail.com>
2024-06-15 19:41:06 +0530

    added public functions

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,64 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
+class Player {
+
+private:
+
+    // Private Properties
+    int health = 3;
+    sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+    int movement_speed = 5;
+    int player_score = 0;
+
+public:
+
+    // Public Properties
+    sf::Texture player_texture;
+    sf::Sprite player_sprite;
+
+    //Public Getter & Setter methods
+    int getScore() {
+        return player_score;
+    };
+
+    void setScore(int newScore) {
+        player_score = newScore;
+    };
+
+    //New methods
+    void takeDamage() {};
+    void move() {};
+    void shootBullets() {};
+
+};
+
+int main() {
+
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow window(videoMode, "SFML Window");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        Player player;
+
+        
+
+        // Clear the window
+        window.clear(sf::Color::Blue);
+
+        // Display whatever you draw
+        window.display();
+    }
+
     return 0;
 }
