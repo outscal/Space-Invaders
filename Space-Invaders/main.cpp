a1c92acc2604520e1a523c27a334ca715c529df7
amichellegarza <amichelle.garza@gmail.com>
2024-06-18 12:13:40 -0500

    Starting off with the player assignment
    Declare the following properties:
    
    sf::texture and sf::sprite
    int player score
    int health
    int movement speed
    sf::Vector2f position
    
    Declare the following methods:
    
    void takeDamage()
    void move()
    void shootBullets()

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -27,15 +27,44 @@ class Player {
         void shootBullets() {}
 };
 
-int main()
-{
-    //Creating player object
-    Player player;
+    int main() {
 
-    //Access the private variable using the public getter
-    std::cout << "Player Score:" << player.getScore() << "\\n";
-    //Modify the variable using the public setter
-    player.setScore(100);
-    //Access the modified variable using the public getter
-    std::cout << "Player Modified Score:" << player.getScore() << "\\n";
-}  
+        sf::VideoMode videoMode = sf::VideoMode(800, 600); // Define the video mode (dimensions)
+
+        sf::RenderWindow window(videoMode, "SFML Window");  // Create a window object
+
+        Player player;  // Create the player object
+
+        player.player_texture.loadFromFile("assets/textures/player_ship.png"); // Load the player ship texture
+
+        player.player_sprite.setTexture(player.player_texture);  // Set the player sprite variable 
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                // Check for window closure
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            // Handle keyboard input
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                player.move();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                player.move();
+            }
+
+            // Clear the window
+            window.clear(sf::Color::Blue); // this code will set a blue background color (optional)
+
+            player.player_sprite.setPosition(player.getPosition()); // Set the position of the player sprite
+
+            window.draw(player.player_sprite);    // Draw the player sprite
+
+            window.display(); // Display what was drawn
+
+        } // end while loop
+
+        return 0;
+    }
