3993f7a5d9644f9b266f340876304fede6a9a371
Divyansh2045 <divyanshnagar@hotmail.com>
2024-06-18 13:08:46 +0530

    Created a window with different parameters
    Created a window with different parameters like width height, name.
    Tried limiting the frame limit and have some commented codes that I may require in the future

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,57 @@
+#include <iostream>
+using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main()
-{
+// namespace sf {
+    
+
+  //  class VideoMode {
+
+    //   public:
+
+    //    VideoMode(int width, int height) {
+
+    //    }
+
+   // };
+ //   class RenderWindow {
+
+ //   public:
+
+  //      RenderWindow(VideoMode videoMode, const char* name) {
+
+  //      }
+ //   };
+//}
+//void gameloop() {
+    
+//}
+
+
+
+int main() {
+
+// Define the video mode (dimensions)
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+
+    sf::RenderWindow window(videoMode, "SFML Window");
+  //  gameloop();
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        }
+    window.setFramerateLimit(60);
+
+
+        window.clear(sf::Color::Blue);
+
+       
+        window.display();
+
+
     return 0;
 }
