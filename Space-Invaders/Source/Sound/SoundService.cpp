af92731137e14f2f0cba638a5fbb594846d599e3
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2024-06-18 20:31:19 -0400

    Created Sound Service

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,33 @@
+#include "../../Header/Sound/SoundService.h"
+
+namespace Sound
+{
+	SoundService::SoundService()
+	{
+	}
+
+	SoundService::~SoundService()
+	{
+	}
+
+	void SoundService::playSound(SoundType type)
+	{
+	}
+
+	void SoundService::backgroundSound()
+	{
+	}
+
+	void SoundService::initialize()
+	{
+	}
+
+	void SoundService::loadBackgroundMusic()
+	{
+	}
+
+	void SoundService::loadSoundMusic()
+	{
+	}
+}
+
