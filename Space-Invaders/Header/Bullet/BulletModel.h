6fe4ea467ed48640fefa41c75bc5ff79093bf4b0
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2024-06-19 12:58:52 -0400

    Created The Model And View

--- Space-Invaders/Header/Bullet/BulletModel.h
+++ Space-Invaders/Header/Bullet/BulletModel.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+namespace Bullet
+{
+	enum class BulletType;
+	enum class MovementDirection;
+	class BulletModel
+	{
+	public:
+		BulletModel(BulletType type);
+		~BulletModel();
+
+		void initialize(sf::Vector2f position, Bullet::MovementDirection direction);
+		void update();
+		void render();
+
+	private:
+		float movement_speed = 300.f;
+		sf::Vector2f bullet_position;
+
+		BulletType bullet_type;
+		MovementDirection movement_type;
+
+		float getMovementSpeed();
+		void setMovementSpeed(float speed);
+
+		sf::Vector2f getPosition();
+		void setPosition(sf::Vector2f psoition);
+
+		BulletType getBulletType();
+		void setBulletType(BulletType type);
+
+		MovementDirection getMovementDirection();
+		void setMovementDirection(MovementDirection direction);
+	};
+}
+
+
--- Space-Invaders/Header/Bullet/BulletView.h
+++ Space-Invaders/Header/Bullet/BulletView.h
@@ -1,1 +1,36 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+
+namespace Bullet 
+{
+	enum class BulletType;
+	class BulletController;
+
+	class  BulletView
+	{
+	public:
+		BulletView();
+		~BulletView();
+
+		void initialize(BulletController* controller);
+		void update();
+		void render();
+
+	private:
+		const float bullet_height = 18.f;
+		const float bullet_width = 18.f;
+
+		sf::Texture bullet_texture;
+		sf::Sprite bullet_sprite;
+
+		sf::RenderWindow* game_window;
+
+		BulletController* bullet_controller;
+
+		void initializingSprite(BulletType type);
+		void scalingSprite();
+	};
+
+}
+
