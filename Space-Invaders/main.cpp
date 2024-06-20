bd7e9adcbe5736fb1fef87e7327b606b6e6846af
Divyansh2045 <divyanshnagar@hotmail.com>
2024-06-20 14:20:14 +0530

    added player movement and player sprite
    Added code for player movement inside main and player sprite is now displayed on screen

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -41,38 +41,53 @@ public:
 
 int main()
 {
-    Player player;
     //render a blue colored window
     sf::VideoMode videomode = sf::VideoMode(800, 600);
     sf::RenderWindow window(videomode, "SFML Window");
+    Player player;
 
-   /* sf::Texture playership;
+    sf::Texture playership;
     playership.loadFromFile("assets/textures/player_ship.png");
     sf::Sprite playershipsprite;
     playershipsprite.setTexture(playership);                                // sprite.setTexture(texture);
     // set position, rotation, scale and more
 
-    playershipsprite.setPosition(100, 200);
+    playershipsprite.setPosition(300, 450);
     playershipsprite.setRotation(0);
-    playershipsprite.setScale(1, 1); */
+    playershipsprite.setScale(1, 1); 
     cout << "Player score is " << player.getScore() << endl;;
     player.setScore(100);
     cout <<"New player score: "<<player.getScore() << endl;
     while (window.isOpen())
     {
         sf::Event event;
-            while (window.pollEvent(event))
+        while (window.pollEvent(event))
+        {
+            //Check for window closure
+            if (event.type == sf::Event::Closed)
             {
-                //Check for window closure
-                if (event.type == sf::Event::Closed)
-                    window.close();
+                window.close();
             }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+
+                player.move();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+
+                player.move();
+            }
+
+
             //Clear the window
             window.clear(sf::Color::Blue);
+            //Draw assets
+            window.draw(playershipsprite);
             //Display window to display whatever drawn
             window.display();
-            //Draw assets
-           // window.draw(playershipsprite);
+        }
 
 
     }
