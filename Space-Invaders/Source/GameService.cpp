effb2a53da59317f55eae5eaac1caa866ca461a7
Divyansh2045 <divyanshnagar@hotmail.com>
2024-06-21 13:47:27 +0530

    Introduced the gameservice class and headerfile
    made 2 new folders headers and source files. Included game service header file and game service cpp file source file.
    Added methods and functions in both files.

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "../HeaderFiles/GameService.h"
+using namespace std;
+
+
+	void GameService::initialize() // initialization of game 
+	{
+
+	}
+	void GameService::destroy() // Destroys game objects when destroyed 
+	{
+
+	}
+	void GameService::iz() {
+
+	}
+	GameService::GameService() // Default Constructor 
+	{
+		//gameloop
+	}
+	GameService::~GameService() // Deconstructor
+	{
+		// end of game when player exits
+	}
+
+	void GameService::update() //Updates the game objects each frame 
+	{
+
+	}
+	void GameService::ignite() // Initiates the game 
+	{
+	}
+	void GameService::render() { //Renders each object each frame
+
+	}
+	bool GameService::isRunning()  // Keeps on checking if the game is running
+	{
+
+		return false;
+	}
+
+
+	
+
+	
