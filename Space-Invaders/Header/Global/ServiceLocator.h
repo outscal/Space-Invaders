7ca0e16743c2673e53a6708c086de8efee47fd03
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-06-25 07:17:32 +0530

    GamePlayUpdated

--- Space-Invaders/Header/Global/ServiceLocator.h
+++ Space-Invaders/Header/Global/ServiceLocator.h
@@ -1,70 +1,45 @@
 #pragma once
-#include "../../header/Graphic/GraphicService.h"
+#include "../../Header/Graphic/GraphicService.h"
 #include "../../header/Event/EventService.h"
-#include "../../header/Player/PlayerService.h"
-#include "../../header/Global/ServiceLocator.h"
-#include "../../header/Time/TimeService.h"
 #include "../../header/UI/UIService.h"
-#include"../../header/Enemy/EnemyService.h"
+#include "../../header/Player/PlayerService.h"
+#include "../../header/Time/TimeService.h"
+#include "../../header/Enemy/EnemyService.h"
+#include "../../header/Gameplay/GameplayService.h"
 
-// ServiceLocator Class Summary: This class manages access to various services in the application.
-// include relevant headers files
-
-
-namespace Global {
+namespace Global
+{
     class ServiceLocator
     {
     private:
-
+        Event::EventService* event_service;
         Graphic::GraphicService* graphic_service;
         UI::UIService* ui_service;
-        Event::EventService* event_service;
-        Player::PlayerService* player_service;
         Time::TimeService* time_service;
+        Gameplay::GameplayService* gameplay_service;
+        Player::PlayerService* player_service;
         Enemy::EnemyService* enemy_service;
 
-        
-        
+        ServiceLocator();
+        ~ServiceLocator();
 
-        // Private Attributes:
-        // - event_service: Manages event-related functionalities.
-        // - graphic_service: Handles graphics-related tasks.
-        // ..........................
-        // ..........................
-
-        // Private Constructor and Destructor:
-
-        // Constructor for initializing the ServiceLocator.
-           ServiceLocator();
-
-            // Destructor for cleaning up resources upon object deletion.
-            ~ServiceLocator();
-
-        // Private Methods:
-
-        void createServices(); 			// Creates instances of all services.
-        void clearAllServices(); 		//	Deletes and deallocates memory for all services.
-
-
+        void createServices();
+        void clearAllServices();
 
     public:
-        // Public Methods:
-        static ServiceLocator* getInstance();  // Provides a method to access the unique ServiceLocator instance (object). We will discuss this later.
+        static ServiceLocator* getInstance();
 
-        void initialize(); 			//	Initializes the ServiceLocator.
-        void update(); 				//	Updates all services.
-        void render(); 				//	Renders using the services.
+        void initialize();
+        void update();
+        void render();
+
+        Event::EventService* getEventService();
         Graphic::GraphicService* getGraphicService();
         UI::UIService* getUIService();
-        Event::EventService* getEventService();
-        Player::PlayerService* getPlayerService();
         Time::TimeService* getTimeService();
-      
-      
-
-        // Methods to Get Specific Services: 
-          // Retrieve the EventService instance
-       // GraphicService* getGraphicService();   // Retrieve the GraphicService instance
-
+        Gameplay::GameplayService* getGameplayService();
+        Player::PlayerService* getPlayerService();
+        Enemy::EnemyService* getEnemyService();
+        void deleteServiceLocator();
     };
 }
