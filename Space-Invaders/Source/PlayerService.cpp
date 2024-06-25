52e783b7293709f5d2f0ed76f183aa59569bce8e
Mirza_4565 <mirzaalishaik2000@gmail.com>
2024-06-25 12:55:03 +0530

    Player_Controller
     Responsible for handling all the logic.

--- Space-Invaders/Source/PlayerService.cpp
+++ Space-Invaders/Source/PlayerService.cpp
@@ -1,86 +1,113 @@
+//#include "../Header/PlayerService.h"
+//#include "../Header/ServiceLocator.h"
+//
+//PlayerService::PlayerService()
+//{
+//	game_window = nullptr;
+//}
+//
+//PlayerService::~PlayerService() = default;
+//
+////init
+//void PlayerService::initialize()
+//{
+//	game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+//	initializePlayerSprite();
+//}
+//
+////take our players input in update, then set the position.
+////order is important here
+//void PlayerService::update()
+//{
+//	processPlayerInput();
+//	player_sprite.setPosition(getPlayerPosition());
+//}
+//
+//void PlayerService::render()
+//{
+//	game_window->draw(player_sprite);
+//}
+//
+////void PlayerService::processPlayerInput()
+////{
+////	// Handle keyboard input
+////	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+////		move(-1.0f * getMoveSpeed());
+////	}
+////	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+////		move(1.0f * getMoveSpeed());
+////	}
+////}
+//
+////changed Player Input
+//void PlayerService::processPlayerInput()
+//{
+//	EventService* event_service = ServiceLocator::getInstance()->getEventService();
+//
+//	if (event_service->isKeyboardEvent()) //check if a key has been pressed
+//	{
+//		if (event_service->pressedLeftKey())
+//		{
+//			moveLeft();
+//		}
+//
+//		if (event_service->pressedRightKey())
+//		{
+//			moveRight();
+//		}
+//	}
+//}
+//
+//// New movement methods
+//void PlayerService::moveLeft()
+//{
+//	position.x -= movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+//}
+//
+//void PlayerService::moveRight()
+//{
+//	position.x += movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+//}
+//
+//void PlayerService::initializePlayerSprite()
+//{
+//	if (player_texture.loadFromFile(player_texture_path))
+//	{
+//		player_sprite.setTexture(player_texture);
+//	}
+//}
+//
+//void PlayerService::move(float offsetX) {
+//	position.x += offsetX*ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+//}
+//
+////helper functions
+//sf::Vector2f PlayerService::getPlayerPosition() { return position; }
+//int PlayerService::getMoveSpeed() { return movement_speed; }
 #include "../Header/PlayerService.h"
-#include "../Header/ServiceLocator.h"
+#include "../Header/Player/PlayerController.h"
 
 PlayerService::PlayerService()
 {
-	game_window = nullptr;
+	player_controller = new PlayerController();
 }
 
-PlayerService::~PlayerService() = default;
+PlayerService::~PlayerService()
+{
+	delete (player_controller);
+}
 
-//init
 void PlayerService::initialize()
 {
-	game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
-	initializePlayerSprite();
+	player_controller->initialize();
 }
 
-//take our players input in update, then set the position.
-//order is important here
 void PlayerService::update()
 {
-	processPlayerInput();
-	player_sprite.setPosition(getPlayerPosition());
+	player_controller->update();
 }
 
 void PlayerService::render()
 {
-	game_window->draw(player_sprite);
+	player_controller->render();
 }
-
-//void PlayerService::processPlayerInput()
-//{
-//	// Handle keyboard input
-//	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-//		move(-1.0f * getMoveSpeed());
-//	}
-//	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-//		move(1.0f * getMoveSpeed());
-//	}
-//}
-
-//changed Player Input
-void PlayerService::processPlayerInput()
-{
-	EventService* event_service = ServiceLocator::getInstance()->getEventService();
-
-	if (event_service->isKeyboardEvent()) //check if a key has been pressed
-	{
-		if (event_service->pressedLeftKey())
-		{
-			moveLeft();
-		}
-
-		if (event_service->pressedRightKey())
-		{
-			moveRight();
-		}
-	}
-}
-
-// New movement methods
-void PlayerService::moveLeft()
-{
-	position.x -= movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-}
-
-void PlayerService::moveRight()
-{
-	position.x += movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-}
-
-void PlayerService::initializePlayerSprite()
-{
-	if (player_texture.loadFromFile(player_texture_path))
-	{
-		player_sprite.setTexture(player_texture);
-	}
-}
-
-void PlayerService::move(float offsetX) {
-	position.x += offsetX*ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-}
-
-//helper functions
-sf::Vector2f PlayerService::getPlayerPosition() { return position; }
-int PlayerService::getMoveSpeed() { return movement_speed; }
