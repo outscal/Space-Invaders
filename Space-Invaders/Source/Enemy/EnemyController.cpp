b9e7754e40bece1852d67fe071a1ebc37932e996
amichellegarza <amichelle.garza@gmail.com>
2024-06-28 13:20:29 -0500

    Fire the Bullets Assignment

--- Space-Invaders/Source/Enemy/EnemyController.cpp
+++ Space-Invaders/Source/Enemy/EnemyController.cpp
@@ -1,12 +1,15 @@
-include "../../Header/Enemy/EnemyController.h"
-#include "../../Header/Enemy/EnemyView.h"
-#include "../../Header/Enemy/EnemyModel.h"
-#include "../../Header/Global/ServiceLocator.h"
+#include "../../header/Enemy/EnemyController.h"
+#include "../../header/Enemy/EnemyView.h"
+#include "../../header/Enemy/EnemyModel.h"
+#include "../../header/Enemy/EnemyConfig.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../header/Bullet/BulletConfig.h"
 
 namespace Enemy
 {
 	using namespace Global;
-
+	using namespace Time;
+	using namespace Bullet;
 
 	EnemyController::EnemyController(EnemyType type)
 	{
@@ -30,6 +33,8 @@ namespace Enemy
 	void EnemyController::update()
 	{
 		move();
+		updateFireTimer(); //new
+		processBulletFire(); //new
 		enemy_view->update();
 		handleOutOfBounds();
 	}
@@ -37,6 +42,20 @@ namespace Enemy
 	void EnemyController::render()
 	{
 		enemy_view->render();
+	}
+
+	void EnemyController::updateFireTimer()
+	{
+		elapsed_fire_duration += ServiceLocator::getInstance()->getTimeService()->getDeltaTime(); //update the elapsed duration
+	}
+
+	void EnemyController::processBulletFire() //if elapsed duration is equal to or more than the amount of time we want to wait until firing than call the fire method.
+	{
+		if (elapsed_fire_duration >= rate_of_fire)
+		{
+			fireBullet();
+			elapsed_fire_duration = 0.f; //set elapsed duration back to 0.
+		}
 	}
 
 	sf::Vector2f EnemyController::getRandomInitialPosition()
@@ -59,63 +78,6 @@ namespace Enemy
 			ServiceLocator::getInstance()->getEnemyService()->destroyEnemy(this);
 		}
 	}
-
-	/*void EnemyController::move()
-	{
-		switch (enemy_model->getMovementDirection())
-		{
-		case::Enemy::MovementDirection::LEFT:
-			moveLeft();
-			break;
-
-		case::Enemy::MovementDirection::RIGHT:
-			moveRight();
-			break;
-
-		case::Enemy::MovementDirection::DOWN:
-			moveDown();
-			break;
-		}
-	}*/
-
-	/*void EnemyController::moveLeft()
-	{
-		sf::Vector2f currentPosition = enemy_model->getEnemyPosition();
-		currentPosition.x -= enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-
-		if (currentPosition.x <= enemy_model->left_most_position.x)
-		{
-			enemy_model->setMovementDirection(MovementDirection::DOWN);
-			enemy_model->setReferencePosition(currentPosition);
-		}
-		else enemy_model->setEnemyPosition(currentPosition);
-	}
-
-	void EnemyController::moveRight()
-	{
-		sf::Vector2f currentPosition = enemy_model->getEnemyPosition();
-		currentPosition.x += enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-
-		if (currentPosition.x >= enemy_model->right_most_position.x)
-		{
-			enemy_model->setMovementDirection(MovementDirection::DOWN);
-			enemy_model->setReferencePosition(currentPosition);
-		}
-		else enemy_model->setEnemyPosition(currentPosition);
-	}
-
-	void EnemyController::moveDown()
-	{
-		sf::Vector2f currentPosition = enemy_model->getEnemyPosition();
-		currentPosition.y += enemy_model->enemy_movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-
-		if (currentPosition.y >= enemy_model->getReferencePosition().y + enemy_model->vertical_travel_distance)
-		{
-			if (enemy_model->getReferencePosition().x <= enemy_model->left_most_position.x) enemy_model->setMovementDirection(MovementDirection::RIGHT);
-			else enemy_model->setMovementDirection(MovementDirection::LEFT);
-		}
-		else enemy_model->setEnemyPosition(currentPosition);
-	}*/
 
 	sf::Vector2f EnemyController::getEnemyPosition()
 	{
