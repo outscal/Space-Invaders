d214c29fcda4ebf684d87f418fa1e1381155d84d
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-06-30 22:08:22 +0530

    AddedSoundFile

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,27 @@
+#include "../../header/Sound/SoundService.h"
+#include"../../Header/Global/ServiceLocator.h"
+
+namespace Sound
+{
+	using namespace Global;
+
+	void SoundService::initialize()
+	{
+	}
+
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+	}
+
+	void SoundService::loadSoundFromFile()
+	{
+	}
+
+	void SoundService::playSound(SoundType soundType)
+	{
+	}
+
+	void SoundService::playBackgroundMusic()
+	{
+	}
+}
