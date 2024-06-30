0a48399f2f8ab2c36b24a7e20685236098679def
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-06-30 21:57:35 +0530

    Commit

--- Space-Invaders/Header/Elements/ElementService.h
+++ Space-Invaders/Header/Elements/ElementService.h
@@ -1,19 +1,34 @@
 #pragma once
-#include <SFML/Graphics.hpp>
-namespace Element {
-	class ElementService {
+#include <vector>
+#include <SFML/System/Vector2.hpp>
+#include "../../header/Elements/Bunker/BunkerController.h"
+#include "../../header/Elements/Bunker/BunkerModel.h"
 
+namespace Element
+{
+	class BunkerController;
 
+	class ElementService
+	{
 	private:
+		//const vector so that the default values will not be changed down the road by mistake.
+		const std::vector<Bunker::BunkerData> bunker_data_list = { Bunker::BunkerData(sf::Vector2f(130.f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(430.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(730.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1130.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1430.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1730.0f, 800.f)) };
+
+		std::vector<Bunker::BunkerController*> bunker_list;
+
+		void destroy();
 
 	public:
 		ElementService();
-		~ElementService();
+		virtual ~ElementService();
 
-		void Update();
-		void Render();
-		void Initialize();
-
-
+		void initialize();
+		void update();
+		void render();
 	};
 }
